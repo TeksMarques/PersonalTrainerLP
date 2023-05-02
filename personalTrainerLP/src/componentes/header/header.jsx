8b7abb32acee0e0d96b6bda1a33d939be273dc9b
TeksMarques <taotaodoce@gmail.com>
2023-05-02 17:18:33 -0300

    feat: adição do componente Header e estilização

--- personalTrainerLP/src/componentes/header/header.jsx
+++ personalTrainerLP/src/componentes/header/header.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './header.scss';
 
 function header() {
   return (
-    <h1>LETICIA SANTOS</h1>
-  )
+    <div className='header'>              
+      <div className='header__container'>
+        <h1>Apaixone-se por cuidar de você mesmo!</h1>
+      </div>
+    </div>
+  );
 }
 
-export default header
-
+export default header;
